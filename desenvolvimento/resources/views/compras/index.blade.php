ed893f4fa475acf7c3d55df53806470fe164759e
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2023-11-02 15:40:32 -0300

    ajustes tela de compra

--- desenvolvimento/resources/views/compras/index.blade.php
+++ desenvolvimento/resources/views/compras/index.blade.php
@@ -3,19 +3,32 @@
 
     <form method="post" action="{{ route('compra.store') }}">
         @csrf
-        <div>
+        <div class="d-flex" style="gap: 20px; flex-wrap: wrap">
             @foreach ($estoques as $estoque)
-                <div class="card d-flex flex-column justify-content-center align-items-center p-2" style="width: 18rem;">
-                    <img class="card-img-top" src="{{ asset('storage/' . $estoque->imagemProduto) }}" alt="Imagem de capa do card" style="max-width: 180px; height: auto">
+                <div
+                    class="card d-flex flex-column justify-content-center align-items-center p-4"
+                    style="width: 18rem; border-radius: 20px; transition: all .2s ease"
+                    onMouseOver="this.style.borderColor='#6c757d'"
+                    onMouseOut="this.style.borderColor='#0000001a'"
+                >
+                    <div style="display: flex; align-items: center; height: 160px">
+                        <img
+                            class="card-img-top"
+                            src="{{ asset('storage/' . $estoque->imagemProduto) }}"
+                            alt="Imagem de capa do card"
+                            style="max-width: 160px; height: auto; max-height: 160px"
+                        >
+                    </div>
+
                     <div class="card-body">
                         <h5 href="{{ route('estoque.index') }}" class="card-title" style="color: #495057">
                             {{ $estoque->nome }}
-                            <div>R$: {{ $estoque->valor_venda }}</div>
-                            <div class="container mt-5">
+                            <div style="font-size: 17px; margin-top: 6px">R$: {{ $estoque->valor_venda }}</div>
+                            <div class="container mt-4">
                                 <div class="input-group">
-                                    <button type="button" class="btn btn-outline-secondary decrease" data-estoque-id="{{ $estoque->id_produto_estoque }}">-</button>
-                                    <input type="text" class="form-control text-center number" name="quantidade[]" value="0" readonly>
-                                    <button type="button" class="btn btn-outline-secondary increase" data-estoque-id="{{ $estoque->id_produto_estoque }}">+</button>
+                                    <button type="button" class="btn btn-outline-secondary decrease" data-estoque-id="{{ $estoque->id_produto_estoque }}" style="border-color: #e9ecef">-</button>
+                                    <input type="text" class="form-control text-center number" name="quantidade[]" value="0" readonly style="background: #fff; border-color: #e9ecef">
+                                    <button type="button" class="btn btn-outline-secondary increase" data-estoque-id="{{ $estoque->id_produto_estoque }}" style="border-color: #e9ecef">+</button>
                                 </div>
                             </div>
                             <input type="hidden" name="estoque_id[]" value="{{ $estoque->id_produto_estoque }}">
@@ -25,12 +38,15 @@
                 </div>
             @endforeach
         </div>
-        <button type="submit" class="btn btn-primary">Finalizar Pedido</button>
+        <div style="display: flex; justify-content: space-between; margin-bottom: 80px;">
+            <footer>
+                <div id="total" style="margin-top: 20px; font-size: 18px; font-weight: bold">Total: <span style="color: green">R$ 0.00</span></div>
+            </footer>
+            <button type="submit" class="btn btn-primary" style="margin-top: 20px">Finalizar Pedido</button>
+        </div>
     </form>
 
-    <footer>
-        <div id="total">Total: R$ 0.00</div>
-    </footer>
+
     <script>
         window.compraStoreRoute = '{{ route("compra.store") }}';
         window.csrfToken = '{{ csrf_token('enikiVJ56pv58gMeH6KFAwuVHsAULgp1FVFLQrga') }}';
