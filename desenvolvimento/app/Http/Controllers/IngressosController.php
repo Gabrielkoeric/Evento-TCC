84221dc1cd8411667e6422ff87432953c6122990
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2023-11-03 20:27:48 -0300

    ajuste validacao ingresso

--- desenvolvimento/app/Http/Controllers/IngressosController.php
+++ desenvolvimento/app/Http/Controllers/IngressosController.php
@@ -5,49 +5,38 @@ namespace App\Http\Controllers;
 use App\Models\Ingressos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
 class IngressosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Request $request)
     {
         $ingressos = DB::table('ingressos')
             ->select('id_ingressos', 'nome', 'descricao', 'quantidade', 'quantidade_disponivel', 'valor')
             ->get();
-
         $mensagemSucesso = $request->session()->get('mensagem.sucesso');
 
         return view('ingressos.index')->with('ingressos', $ingressos)->with('mensagemSucesso', $mensagemSucesso);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('ingressos.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
+        $request->validate([
+            'nome' => 'required|min:3',
+            'descricao' => 'required|min:8',
+            'quantidade' => 'required|numeric',
+            'quantidadeDisponivel' => 'required|numeric',
+            'valor' => 'required|numeric',
+        ]);
+
         $nome = $request->input('nome');
         $descricao = $request->input('descricao');
         $quantidade = $request->input('quantidade');
         $quantidadeDisponivel = $request->input('quantidadeDisponivel');
         $valor = $request->input('valor');
-        //dd("nome é $nome, descrição é $descricao, quantidade é $quantidade, quantidade disponivel é $quantidadeDisponivel, e valor é $valor");
 
         $dados = [
             'nome' => $nome,
@@ -60,38 +49,21 @@ class IngressosController extends Controller
         return redirect('/ingressos')->with('mensagem.sucesso', 'Ingresso inserido com sucesso!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Ingressos $ingresso)
     {
-     //dd("$ingresso");
      return view('ingressos.edit')->with('ingresso', $ingresso);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id_ingressos)
     {
+        $request->validate([
+            'nome' => 'required|min:3',
+            'descricao' => 'required|min:8',
+            'quantidade' => 'required|numeric',
+            'quantidadeDisponivel' => 'required|numeric',
+            'valor' => 'required|numeric',
+        ]);
+
         DB::table('ingressos')
             ->where('id_ingressos', $id_ingressos)
             ->update([
@@ -104,16 +76,9 @@ class IngressosController extends Controller
         return redirect()->route('ingressos.index')->with('mensagem.sucesso', 'Ingresso Alterado com Sucesso!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Ingressos $ingresso)
     {
         $ingresso->delete();
         return to_route('ingressos.index')->with('mensagem.sucesso', 'Ingresso Removido com Sucesso!');
     }
-
 }
--- desenvolvimento/resources/views/produtos/old.index.blade.php
+++ desenvolvimento/resources/views/produtos/old.index.blade.php
@@ -26,8 +26,6 @@
                                 <button type="button" class="btn btn-outline-secondary decrease">-</button>
                             </div>
                             <input type="text" class="form-control text-center number" name="quantidade[{{ $sql->id_produtos_disponiveis }}]" value="0" readonly>
-                            <!--<input type="text" class="form-control text-center number" name="quantidade[{{ $sql->id_produtos_disponiveis }}]" value="0" readonly data-maxQuantity="{{ $sql->quantidade }}">-->
-
                             <div class="input-group-append">
                                 <button type="button" class="btn btn-outline-secondary increase">+</button>
                             </div>
